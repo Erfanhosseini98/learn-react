652d8617663bfeb26143afc0d6ec9a41eea1903c
Erfan Hosseini <efn.hosseini@gmail.com>
2023-06-20 12:56:17 +0330

    content added to Navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,16 @@
 const Navbar = () => {
     return( 
-        <h1>header</h1>
+        <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
+      </div>
+    </nav>
     )
 }
 
