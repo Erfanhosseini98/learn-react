fd189cb33844061dd56acf95ff5dc71fff110f44
Erfan Hosseini <efn.hosseini@gmail.com>
2023-06-21 13:01:26 +0330

    work on mapping and use state for blog block

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,19 +2,28 @@ import { useState } from "react"
 
 const Home = () => {
 
-    const [a, setA] = useState('mario')
-    const [b, setB] = useState(25)
-    
-    const handleClick = () => {
-        setA('C')
-        setB(654)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
 
-    return( 
+    return (
         <>
-            <p>......{a}......{b}</p>
-            <button onClick={handleClick}>Click me</button>
+            <div className="home">
+
+                {
+                    blogs.map((blog) => (
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by {blog.author}</p>
+                        </div>
+                    ))
+                }
+
+
+            </div>
         </>
     )
 }
