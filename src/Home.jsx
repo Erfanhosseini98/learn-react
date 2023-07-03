02d4b5df572268d4d29f50ba9f483278ddece218
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-03 17:06:21 +0330

    loading added to blog

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import BlogList from "./BlogList"
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -11,20 +12,24 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data)
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    setBlogs(data)
+                    setLoading(false)
+                })
+        },1000)
     }, [])
-    
+
     return (
         <>
             <div className="home">
+                {loading && <div>Loooooooooooooooading</div>}
                 {blogs && <BlogList blogs={blogs} title="All Blog" handleDelete={handleDelete} />}
-                {blogs &&  <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario Blog" />}
+                {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario Blog" />}
             </div>
         </>
     )
