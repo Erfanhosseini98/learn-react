3364c75992b5d8abdf6009878c49a4a0fe7859b4
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-03 17:58:01 +0330

    handle server error with useState and catch function

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -15,18 +16,28 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource')
+                    }
+                    console.log(res)
                     return res.json()
                 })
                 .then(data => {
                     setBlogs(data)
                     setLoading(false)
                 })
-        },1000)
+                .catch(e => {
+                    setLoading(false)
+                    setError(e.message)
+
+                })
+        }, 1000)
     }, [])
 
     return (
         <>
             <div className="home">
+                {error && <div>{error}</div>}
                 {loading && <div>Loooooooooooooooading</div>}
                 {blogs && <BlogList blogs={blogs} title="All Blog" handleDelete={handleDelete} />}
                 {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario Blog" />}
