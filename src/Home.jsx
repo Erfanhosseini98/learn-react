59505158efed09d742f0b93f954edde7336b6e17
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-03 16:05:07 +0330

    add json

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,18 +2,28 @@ import { useEffect, useState } from "react"
 import BlogList from "./BlogList"
 
 const Home = () => {
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
-useEffect(() => {
 
-},[])
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            res.json()
+        })
+        .then(() => {
+            
+        })
+    }, [])
+    
     return (
         <>
             <div className="home">
@@ -24,4 +34,4 @@ useEffect(() => {
     )
 }
 
-export default Home
+export default Home 
