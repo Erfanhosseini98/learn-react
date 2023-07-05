00dc50fb87ac04c62b76c6b2cd6c4ce5ca2d4846
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-05 13:33:59 +0330

    add h2 tag for beeter style

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,9 @@
 const Create = () => {
-    return (<div>
-        Create Page
-    </div>)
+    return (
+    <div>
+        <h1>Create a new blog post</h1>
+    </div>
+    )
 }
  
 export default Create
