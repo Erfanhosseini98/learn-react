608e7f98d93a786345736adf2f95e14df8fb7806
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-08 15:16:34 +0330

    link added to each blog for read more

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom"
 
-const BlogList = ({ blogs ,title, handleDelete}) => {
+
+const BlogList = ({ blogs, title, handleDelete }) => {
 
 
   return (
     <>
-    <h3>{title}</h3>
+      <h3>{title}</h3>
       <div className="blog-list">
         {
           blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
-              <h2>{blog.title}</h2>
-              <p>Written by {blog.author}</p>
-              <br />
-              {handleDelete && <button onClick={() => handleDelete(blog.id)}>delete blog</button>}
+              <Link to={`Blog/${blog.id}`}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <br />
+                {handleDelete && <button onClick={() => handleDelete(blog.id)}>delete blog</button>}
+              </Link>
             </div>
           ))
         }
