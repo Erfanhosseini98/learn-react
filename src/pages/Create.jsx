d065f67fd3016ef22c612e54a9935918db31dd58
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-08 19:14:06 +0330

    add input to create blog component

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react"
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('Erfan')
+
+
     return (
-    <div>
-        <h1>Create a new blog post</h1>
-    </div>
+        <>
+            <div className="create" >
+                <h2>Create a new blog post</h2>
+                <form action="">
+
+                    <label>Blog Title:</label>
+                    <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <label>Blog Body:</label>
+                    <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                    <label>Blog author:</label>
+                    <select
+                        value={author} onChange={(e) => setAuthor(e.target.value)}>
+                        <option value="Erfan">Erfan</option>
+                        <option value="Eric">Eric</option>
+                    </select>
+                    <button >
+                        Add Blog
+                    </button>
+                    <p>{title}</p>
+                    <p>{body}</p>
+                    <p>{author}</p>
+
+                </form>
+            </div>
+
+        </>
+
     )
 }
- 
+
 export default Create
