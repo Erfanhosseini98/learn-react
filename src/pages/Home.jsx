c7cea36599aadc4be09a4008a3a052f618f2ae0d
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-17 19:05:05 +0330

    Used reamtch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,33 @@
 
-import BlogList from "../BlogList"
 import useFetch from "../useFetch"
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from "react"
+import BlogItem from "../components/BlogItem"
 
 const Home = () => {
-
-    const { error, loading, data: blogs } = useFetch('http://localhost:8000/blogs')
-
-
-
+    // const { error, loading, data: blogs } = useFetch()
+    const dispatch = useDispatch()
+    const blogs = useSelector((store) => store.MyBlogs.data)
+    const error = useSelector((store) => store.MyBlogs.error)
+    const loading = useSelector((store) => store.MyBlogs.loading)
+    useEffect(() => {
+        dispatch.MyBlogs.getBlogs()
+    }, [])
 
     return (
         <>
             <div className="home">
-             
+                <h3>All Blogs</h3>
                 {error && <div>{error}</div>}
-                {loading && <div>Loooooooooooooooading</div>}
-                {blogs && <BlogList blogs={blogs} title="All Blog" />}
+                {loading && <div>Loading...</div>}
+                {blogs &&
+                    <div className="blog-list">
+                        {
+                            blogs.map((blog) => (
+                                <BlogItem key={blog.id} blog={blog}></BlogItem>
+                            ))
+                        }
+                    </div>}
             </div>
         </>
     )
