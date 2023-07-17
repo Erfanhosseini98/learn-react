a5f631ad5f78f2312980e65abf811c743498ecba
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-17 19:03:28 +0330

    changed directory

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Erfan Blog</h1>
+      <div className="links">
+        <NavLink  to="/">Home</NavLink >
+        <NavLink  to="/create" >New Blog</NavLink >
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
