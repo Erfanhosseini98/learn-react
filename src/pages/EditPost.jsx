90c44e27468925ccfd0d4a39a79deab8dc359f0e
Erfan Hosseini <efn.hosseini@gmail.com>
2023-07-17 19:04:49 +0330

    implemented the eadt compoenent

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,35 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import useFetch from "../useFetch"
 
 const EditPost = () => {
     const { id } = useParams()
-    const { data: blog, loading: blogLoading , error } = useFetch('http://localhost:8000/blogs/' + id)
-    console.log(blog)
+    const { data: blog, loading: blogLoading, error } = useFetch('http://localhost:8000/blogs/' + id)
     const { data: authors, loading: authorsLoading, error: authorsError } = useFetch('http://localhost:8000/authors')
     const [body, setBody] = useState('')
     const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    // const [loading, setLoading] = useState(false)
-    // const navigate = useNavigate()
-    // const handleEdit = (e) => {
-    //     e.preventDefault()
-    //     const blog = { title, body, author }
-    //     setLoading(true)
-    //     fetch('http://localhost:8000/blogs' + id, {
-    //         method: 'PATCH',
-    //         headers: { "Content-Type": "application/json" },
-    //         body: JSON.stringify(blog)
-    //     }).then(
-    //         setLoading(() => {
-    //             setLoading(false)
-    //             navigate("/")
-    //         })
-    //     )
-    // }
+    const [authorId, setAuthorId] = useState('')
+    useEffect(() => {
+        if (blog) {
+            setAuthorId(blog.authorId)
+            setTitle(blog.title)
+            setBody(blog.body)
+        }
+    },[blog])
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+    const handleEdit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, authorId }
+        setLoading(true)
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'PATCH',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(
+            setLoading(() => {
+                setLoading(false)
+                navigate("/")
+            })
+        )
+    }
     return (
         <>
-            {/* <div className="create" >
+            <p>{title}</p>
+            <div className="create" >
                 <h2>Edit </h2>
                 <form onSubmit={handleEdit}>
 
@@ -38,18 +45,17 @@ const EditPost = () => {
                     <label>Blog Body:</label>
                     <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
                     <label>Blog author:</label>
-                    <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <select value={authorId} onChange={(e) => setAuthorId(+e.target.value)}>
                         {authorsError && <option>{authorsError}</option>}
                         {authorsLoading && <option>Loading...</option>}
                         {authors && authors.map((item) => {
-                            { console.log(item) }
                             return <option key={item.id} value={item.id} >{item.name}</option>
                         })}
                     </select>
-                    {!loading && <button >Add Blog</button>}
-                    {loading && <button disabled >Adding Blog ...</button>}
+                    {!loading && <button >Edit Blog</button>}
+                    {loading && <button disabled >Editing Blog ...</button>}
                 </form>
-            </div> */}
+            </div>
         </>
     )
 }
