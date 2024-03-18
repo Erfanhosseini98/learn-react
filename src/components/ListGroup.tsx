480fc9cbc6e22494d84c2cd26b98df52a79562ab
Erfan Hosseini <efn.hosseini@gmail.com>
2024-03-18 20:27:22 +0000

    Implemented dynamic menu activation using hooks.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 function ListGroup() {
+	const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+	const [selectedIndex, setSelectedIndex] = useState(-1);
 	return (
-		<ul className="list-group">
-			<li className="list-group-item">An item</li>
-			<li className="list-group-item">A second item</li>
-			<li className="list-group-item">A third item</li>
-			<li className="list-group-item">A fourth item</li>
-			<li className="list-group-item">And a fifth one</li>
-		</ul>
+		<>
+			{items.length === 0 && <p>No item found</p>}
+			<ul className="list-group">
+				{items.map((item, index) => (
+					<li
+						key={item}
+						className={
+							selectedIndex === index
+								? "list-group-item active"
+								: "list-group-item "
+						}
+                        onClick={() => setSelectedIndex(index)}
+					>
+						{item}
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
