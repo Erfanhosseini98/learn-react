0547181980db0739502f97991dfc79d2d6ecac58
Erfan Hosseini <efn.hosseini@gmail.com>
2024-03-27 19:38:38 +0000

    Refactor: Create separate file for Category data for better modularity

---
+++ src/expense-tracker/categories.ts
@@ -0,0 +1,14 @@
+const categories = [
+	"Housing",
+	"Utilities",
+	"Food",
+	"Transportation",
+	"Healthcare",
+	"Insurance",
+	"Entertainment",
+	"Education",
+	"Savings",
+	"Debt Repayment",
+] as const;
+
+export default categories
